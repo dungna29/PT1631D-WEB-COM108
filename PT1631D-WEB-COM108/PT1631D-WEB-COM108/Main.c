dec57515641c1bc008f048975d47e6dc352ebe3c
Nguyễn Anh Dũng <55435241+dungna29@users.noreply.github.com>
2020-10-21 14:29:55 +0700

    Dungna

---
+++ PT1631D-WEB-COM108/PT1631D-WEB-COM108/Main.c
@@ -0,0 +1,18 @@
+﻿#include <stdio.h>
+//Một chương trình C cần phải có hàm Main()
+//Hàm main dùng để chạy khi chương trình được khởi động
+int main()//Đây được gọi là hàm main
+{//Dấu ngoặc dùng để thể hiện 1 khoảng lệnh
+
+
+	//Trong lập trình đã mở lệnh thì luôn phải đóng lệnh
+	
+	
+	//Để in ra màn hình trong C các bạn sử dụng câu lệnh 
+	// printf(Nội dung muốn in ra màn hình);
+	printf("Chao Cac Ban Da Den Voi Lap Trinh C Tai POLY");//Dấu ; dùng để kết thúc 1 câu lệnh
+	//ở đây sẽ in ra màn hình 1 chuỗi ký tự
+
+	//Dùng phím F5 để chạy chương trình khi sử dụng Visual Studio
+	return 0;
+}
