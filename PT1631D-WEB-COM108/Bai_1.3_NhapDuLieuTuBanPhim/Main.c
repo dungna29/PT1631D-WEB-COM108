5d9bf2634362df5299494df2248b8b5eaeda92e8
Nguyễn Anh Dũng <55435241+dungna29@users.noreply.github.com>
2020-10-23 15:56:00 +0700

    Toan tu

---
+++ PT1631D-WEB-COM108/Bai_1.3_NhapDuLieuTuBanPhim/Main.c
@@ -0,0 +1,31 @@
+﻿#include <stdio.h>
+/// <summary>
+/// Nhập dữ liệu từ bàn phím vào
+/// </summary>
+/// <returns></returns>
+int main()
+{
+	int nam = 2020;// toán tử bằng dùng để gán giá trị
+	printf("Nam Can In Ra La: %d\n", nam);
+	//Nhập dữ liệu từ bàn phím sử dụng hàm scanf hoặc scanf_s(đối với visual studio)
+	//Viết 1 chương trình nhập điểm môn C
+	float diemMonC = 0;//Bước 1 khai báo 1 biến để chưa giá trị khi người dùng nhập vào
+	printf("Moi ban nhap diem tong ket mon C: ");
+	scanf_s("%f", &diemMonC);//diemMonC sẽ chứa giá trị mà người dùng nhập vào từ bàn phím
+	printf("Diem C ban vua nhap vao la: %f \n", diemMonC);
+
+	/*
+	 * Bài 1: Viết 1 chương trình nhập 2 số nguyên và 
+	 * in 2 số đó ra màn hình
+	 * Bài 2: Viết 1 chương trình nhập 2 số thực và
+	 *  in 2 số đó ra màn hình
+	 */
+	//Giải bài 2	
+	int a = 0, b = 0;
+	printf("Moi ban nhap so thu 1: \n");
+	scanf_s("%d", &a);
+	printf("Moi ban nhap so thu 2: \n");
+	scanf_s("%d", &b);
+	printf("Hai so ban vua nhap %d va %d \n", a, b);
+	return 0;
+}
---
+++ PT1631D-WEB-COM108/Bai_1.4_ToanTuSoHoc/Main.c
@@ -0,0 +1,28 @@
+﻿#include <stdio.h>
+/// <summary>
+/// Toán tử số học trong lập trình 
+///	+ - * / (cộng,trừ,nhân,chia) ++(tăng số lên 1 đơn vị) --(giảm 1 đơn vị) %(chia lấy phần dư)
+/// </summary>
+/// <returns></returns>
+int main()
+{
+	int a = 9, b = 8, c = 0;//Dùng c để lưu trữ kết quả
+	//Phép toán cộng trừ nhân chia
+	c = a + b;
+	printf("%d + %d = %d \n", a, b, c);//Cách viết 1
+	printf("%d + %d = %d \n", a, b, a + b);//Cách viết 2
+	a++;//Tăng 1 đơn vị 9 tăng lên 10
+	b--;//Giảm 1 đơn vị 8 giảm xuống 7
+	printf("%d + %d = %d \n", a, b, a + b);
+	//Chia lấy phần dư
+	printf("Chia lay phan du 8 phan tram 2 = %d \n", 8 % 2);
+	printf("Chia lay phan du 8 phan tram 3 = %d \n", 8 % 3);
+
+	/*
+	 * Viết 1 chương trình cho người dùng nhập vào 3 số thực
+	 * Sau đó in ra 4 phép toán cộng trừ nhân chia của 3 số đó
+	 * In ra màn hình các số đã nhập vào và kết quả* 
+	 * 
+	 */
+	return 0;
+}
