e70062c78684ab15dee06a07910e31d090c70320
Nguyễn Anh Dũng <55435241+dungna29@users.noreply.github.com>
2020-11-17 20:11:17 +0700

    1

---
+++ PT1631D-WEB-COM108/BAI_ON_TAP_DI_THI/Main.c
@@ -0,0 +1,14 @@
+﻿#include <stdio.h>
+/// <summary>
+/// BÀI ASSIGNMENT PHẦN 1
+/// </summary>
+/// <returns></returns>
+int main()
+{
+	/*
+	 * Viết 1 chương trình gồm các chức năng sau: (Menu SwitchCase)
+	 * Bài 1: Nhập thông tin học sinh (Tên học sinh, Tuổi, Điểm C, Thành Phố)
+	 * Bài 2: Nhập một tập hợp các số nguyên và in các số chẵn ra màn hình
+	 * Bài 3: Chương trình đăng nhập User và Pass (Tạo sẵn User = admin, Pass = abc!)
+	 */
+}
